7d7e4936f9c14f2ed58a677b3a9cd1d82073e350
PHÚC TRẦN HỒNG MINH <20127285@student.hcmus.edu.vn>
2021-07-13 23:57:42 +0700

    next

--- project MMT/project MMT.cpp
+++ project MMT/project MMT.cpp
@@ -1,20 +1,17 @@
-// project MMT.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include"Header.h"
 
-#include <iostream>
-
-int main()
+int main(int argc, char const* argv[])
 {
-    std::cout << "Hello World!\n";
+	int server_fd;
+	int new_socket; // cho s
+	int valread;	// bien doc
+
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof{address};
+	char buffer[1024] = { 0 };
+	char* hello = "hello from server";
+
+	// creating socket file 
+	if((server_fd = socket()))
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

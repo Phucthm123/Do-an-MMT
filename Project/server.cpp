337eff95caec9d00a4f9568d6f74de3785ca0f37
PHÚC TRẦN HỒNG MINH <20127285@student.hcmus.edu.vn>
2021-07-27 21:30:46 +0700

    new

---
+++ Project/server.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+void main()
+{
+	// Initialze winsock
+
+	// Create the socket
+
+	// Bind the socket to an IP address and port
+}
